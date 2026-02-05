766990ba83ce09f6182063abeb390386a83373a3
saklab-39th <n.sakuraba@gmail.com>
2026-02-05 19:49:42 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import requests
 
-WEBHOOK_URL = "ここにDiscordのWebhook URLを貼る"
+WEBHOOK_URL = "https://discord.com/api/webhooks/1468905881025642496/Nz9PcITWPC6K7gDG95A0mrqkKaqKnbqXHvs-yxDEW01k5gxKBNHv_d_oNTPwcQDbLbfG"
 
 message = {
     "content": "🤖 Botのテスト通知です。正常に動いています。"
 }
 
-requests.post(WEBHOOK_URL="https://discord.com/api/webhooks/1468905881025642496/Nz9PcITWPC6K7gDG95A0mrqkKaqKnbqXHvs-yxDEW01k5gxKBNHv_d_oNTPwcQDbLbfG", json=message)
+requests.post(WEBHOOK_URL, json=message)
