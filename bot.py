611cd9b92a4664591411e2680d053a70d7872e84
saklab-39th <n.sakuraba@gmail.com>
2026-02-05 20:10:04 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,21 @@
 import requests
 
 WEBHOOK_URL = "https://discord.com/api/webhooks/1468905881025642496/Nz9PcITWPC6K7gDG95A0mrqkKaqKnbqXHvs-yxDEW01k5gxKBNHv_d_oNTPwcQDbLbfG"
-
 URL = "https://p-bandai.jp/item/item-1000135894/"
 
 html = requests.get(URL).text
 
 # ページの中身を少しだけ表示（デバッグ用）
 print(html[:500])
+
+if "再販受付中" in html:
+    print("再販受付中 を見つけました！")
+
+elif "予約受付中" in html:
+    print("予約受付中 を見つけました！")
+
+elif "販売中" in html:
+    print("販売中 を見つけました！")
+
+else:
+    print("まだ再販されていません")
